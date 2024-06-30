6e1dca9fc4f90fed775bf2e1032f087b8c0c494c
nitesh sah <niteshsah671@gmail.com>
2024-06-30 13:25:24 +0530

    postman basic, different https methos for communication and resassure basic completed

---
+++ lernRestAssured/src/payload/Test.java
@@ -0,0 +1,10 @@
+package payload;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print(Payload.updateAddress("123456", "xxxx"));
+	}
+
+}
